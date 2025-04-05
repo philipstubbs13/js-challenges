f641776ba7688e8fd8c9897d9e4ea2d668abad63
Phil Stubbs <philipstubbs13@gmail.com>
2025-04-05 12:33:47 -0500

    num range

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,12 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+  if (startNum === endNum) {
+    return [startNum];
+  }
+
+  const numbers = numberRange(startNum, endNum - 1);
+  numbers.push(endNum);
+
+  return numbers;
+}
 
 module.exports = numberRange;
