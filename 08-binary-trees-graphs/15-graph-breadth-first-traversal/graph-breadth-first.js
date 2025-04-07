f2ffefc7788b28bbf8377f5cbf2121f23801a65c
Phil Stubbs <philipstubbs13@gmail.com>
2025-04-07 07:40:57 -0500

    graphs

--- 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
+++ 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
@@ -1,5 +1,27 @@
-const Queue = require('./queue');
+const Queue = require("./queue");
 
-function breadthFirstTraversal() {}
+function breadthFirstTraversal(graph, startingVertex) {
+  const visited = new Set();
+  const result = [];
+  const queue = new Queue();
+
+  queue.enqueue(startingVertex);
+  visited.add(startingVertex);
+
+  while (!queue.isEmpty()) {
+    const currentVertex = queue.dequeue();
+    result.push(currentVertex);
+
+    for (const neighbor of graph.adjacencyList[currentVertex]) {
+      if (!visited.has(neighbor)) {
+        queue.enqueue(neighbor);
+        visited.add(neighbor);
+      }
+      // Alternatively, you can use the spread operator for the adjacencyList to avoid creating a new array:
+    }
+  }
+
+  return result;
+}
 
 module.exports = breadthFirstTraversal;
